8a6f80eda9ad5d8640e27b02793eacab949e9268
NH-AT13B-AT 若林 <238187822+nhs240037@users.noreply.github.com>
2026-09-18 13:43:00 +0900

    prepare for auto-update (wip)

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
-﻿namespace ProjectCleaner;
+﻿using System.Reflection;
+
+namespace ProjectCleaner;
 
 internal static class Program
 {
   [STAThread]
   private static void Main(string[] args)
   {
+    AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
     Application.EnableVisualStyles();
     Application.SetCompatibleTextRenderingDefault(false);
     Application.Run(new MainForm(args));
+  }
+
+  private static Assembly? CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+  {
+    string assemblyName = new AssemblyName(args.Name).Name + ".dll";
+    string libPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "lib", assemblyName);
+
+    if (File.Exists(libPath))
+      return Assembly.LoadFrom(libPath);
+
+    return null;
   }
 }
 
